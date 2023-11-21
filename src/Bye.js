667f991e4f4d82ec5ac70e9a001d8f3aea98ea58
Naveen <kumarnav909@gmail.com>
2023-11-21 23:31:36 +0530

    lab3 done

---
+++ src/Bye.js
@@ -0,0 +1,8 @@
+// Bye.js
+import React from 'react';
+
+function Bye() {
+  return <p>Goodbye, Naveen K!</p>;
+}
+
+export default Bye;
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+// Hello.js
+import React from 'react';
+
+function Hello() {
+  return <p>Hello, Naveen!</p>;
+}
+
+export default Hello;
